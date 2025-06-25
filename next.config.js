9331eefd2319c7785fd285363c71538f8d8b114c
Mahendra Kumawat <117855423+mk016@users.noreply.github.com>
2025-06-25 22:20:39 +0530

    Fix Next.js configuration to resolve bundler error

--- next.config.js
+++ next.config.js
@@ -13,53 +13,6 @@ const nextConfig = {
       },
     ],
   },
-  // Completely disable SWC to avoid native addon issues
-  swcMinify: false,
-  compiler: {
-    // Disable SWC compiler entirely
-    removeConsole: false,
-  },
-  experimental: {
-    // Disable experimental features that might use SWC
-    forceSwcTransforms: false,
-    swcTraceProfiling: false,
-    swcPlugins: [],
-  },
-  webpack: (config, { dev, isServer }) => {
-    // Force use of Babel instead of SWC
-    config.module.rules.forEach((rule) => {
-      if (rule.use && Array.isArray(rule.use)) {
-        rule.use.forEach((useItem) => {
-          if (useItem.loader && useItem.loader.includes('next-swc-loader')) {
-            useItem.loader = 'babel-loader';
-            useItem.options = {
-              presets: ['next/babel'],
-            };
-          }
-        });
-      }
-    });
-
-    if (dev) {
-      // Optimize for development
-      config.optimization = {
-        ...config.optimization,
-        splitChunks: {
-          chunks: 'all',
-          maxSize: 200000,
-          cacheGroups: {
-            default: {
-              minChunks: 1,
-              priority: -20,
-              reuseExistingChunk: true,
-            },
-          },
-        },
-      };
-    }
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
