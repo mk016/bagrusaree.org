4251895ab06e2dd0cf82134d2a3da2bb513a5766
mk016 <mahendrakumawat80224@gmail.com>
2025-08-25 22:01:16 +0530

    Clean up temporary script files

--- fix_all_auth.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Fixing all auth issues..."
-
-# Fix auth calls in all API routes
-find app/api -name "*.ts" -exec sed -i '' 's/const { userId } = await auth();/\/\/ TODO: Fix authentication setup - currently bypassing for functionality/' {} \;
-find app/api -name "*.ts" -exec sed -i '' 's/const authResult = await auth();/\/\/ TODO: Fix authentication setup - currently bypassing for functionality/' {} \;
-
-echo "Auth issues fixed! Now committing and pushing..."
